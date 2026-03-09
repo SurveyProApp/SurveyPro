5d400b23eadcfed9b1e82a1d0f3fb06c9ffc2de6
Yuriy Fundak <yfundak@gmail.com>
2026-03-09 20:29:37 +0200

    add dbcontext class

---
+++ src/SurveyPro.Infrastructure/Persistence/SurveyProDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyPro.Infrastructure.Persistence
+{
+    public class SurveyProDbContext : DbContext
+    {
+        public SurveyProDbContext(DbContextOptions<SurveyProDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
