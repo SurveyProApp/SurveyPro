372382a731f4f752fc780f9d7db0a03661e68ffb
Overko Yulian <YULIAN-ILLIA.OVERKO@lnu.edu.ua>
2026-03-10 16:18:50 +0200

    Added logger

--- src/SurveyPro.Web/Program.cs
+++ src/SurveyPro.Web/Program.cs
@@ -1,7 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using SurveyPro.Infrastructure.Persistence;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// конфігурація Serilog
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Application", "SurveyPro")
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.Seq("http://localhost:5341")
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+Log.Information("SurveyPro application started");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,11 +31,15 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
 }
 
+// логування HTTP запитів
+app.UseSerilogRequestLogging();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
